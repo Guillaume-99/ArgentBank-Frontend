5ae292b0579aee4c305952ea11f5fc1c88e3936e
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-04-28 12:37:13 +0200

    feat(header): add functional header

--- src/components/Header/HeaderIndex.jsx
+++ src/components/Header/HeaderIndex.jsx
@@ -1,17 +1,17 @@
-import "./header.scss";
+import "./HeaderIndex.scss";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
-            <nav className="main-nav">
-                <NavLink className="main-nav-logo" to="/">
-                    <img className="main-nav-logo-image" src="/images/argentBankLogo.png" alt="Argent Bank Logo" />
+            <nav className="header__nav">
+                <NavLink className="header__nav--logo" to="/">
+                    <img className="header__nav--logo-image" src="/images/argentBankLogo.png" alt="Argent Bank Logo" />
                     <h1 className="sr-only">Argent Bank</h1>
                 </NavLink>
 
                 <div>
-                    <NavLink className="main-nav-item" to="/signin">
+                    <NavLink className="header__nav--item" to="/signIn">
                         <i className="fa fa-user-circle"></i>
                         Sign In
                     </NavLink>
