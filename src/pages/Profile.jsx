869b35257405e38918a042396f21d1c5597823a3
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-05-04 12:03:56 +0200

    feat(Profile) add profile pages visual

--- src/pages/Profile.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <main>Profile page</main>;
-}
-
-export default Home;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home/Home.jsx";
 import SignIn from "../pages/SignIn/SignIn.jsx";
-import Profile from "../pages/Profile.jsx";
+import Profile from "../pages/Profile/Profile.jsx";
 
 const router = createBrowserRouter([
     {
